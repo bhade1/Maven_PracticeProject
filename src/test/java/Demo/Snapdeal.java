17c65e39bb80e22fbea2c198de87e2e140afc7e0
bhade1 <kiranbhade888@gmail.com>
2023-07-31 21:05:20 +0530

    [bhade1] Adding new TC --> Snapdeal

--- src/test/java/Demo/Snapdeal.java
+++ src/test/java/Demo/Snapdeal.java
@@ -1,10 +1,23 @@
 package Demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Snapdeal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.snapdeal.com/");
+		Thread.sleep(3000);
+		driver.quit();
+		
+		
+		
+		
 	}
 
 }
